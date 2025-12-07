a76e5de62ea5d19b1e3a9bb2b2d324d3d5d60ce8
Victor Kovalchuk <vict02r05@gmail.com>
2025-12-07 17:17:21 +0200

    lecture 31

---
+++ 031-waapi-iteration-composite/web-api/script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('.square');
+
+  const squareAnimation = element.animate(
+    [
+      {
+        transform: 'translateX(0)',
+        easing: 'ease-in'
+      },
+      {
+        backgroundColor: 'blue',
+        offset: 0.8,
+        composite: 'replace'
+      },
+      {
+        transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+        backgroundColor: 'crimson',
+      }
+    ],
+    {
+      duration: 3000,
+      delay: 1000,
+      direction: 'normal',
+      fill: 'both',
+      iterations: Infinity,
+      easing: 'linear',
+      composite: 'add',
+      iterationComposite: 'accumulate',
+      timeline: document.timeline
+    }
+  );
+
+  // squareAnimation.pause();
+});
