546c55f61f6e68ddaf65826534be3f69e8346272
Victor Kovalchuk <vict02r05@gmail.com>
2025-12-07 17:47:46 +0200

    lecture 32

---
+++ 032-waapi-animation-object-methods/web-api/script.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const element = document.querySelector('.square');
+
+  const squareAnimation = element.animate(
+    [
+      {
+        transform: 'translateX(0)',
+        easing: 'ease-in'
+      },
+      {
+        backgroundColor: 'blue',
+        offset: 0.8,
+        composite: 'replace'
+      },
+      {
+        transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+        backgroundColor: 'crimson',
+      }
+    ],
+    {
+      duration: 3000,
+      // delay: 1000,
+      direction: 'alternate',
+      fill: 'both',
+      iterations: Infinity,
+      easing: 'linear',
+      composite: 'add',
+      iterationComposite: 'accumulate',
+      timeline: document.timeline
+    }
+  );
+
+  console.log(squareAnimation);
+
+  squareAnimation.pause();
+
+  const buttons = document.querySelectorAll('.button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('play')) {
+        squareAnimation.play();
+      }
+      if (button.classList.contains('pause')) {
+        squareAnimation.pause();
+      }
+      if (button.classList.contains('cancel')) {
+        squareAnimation.cancel();
+      }
+      if (button.classList.contains('reverse')) {
+        squareAnimation.reverse();
+      }
+      if (button.classList.contains('finish')) {
+        squareAnimation.finish();
+      }
+    });
+  });
+
+  const playbackRateInput = document.getElementById('playbackRateInput');
+  const playbackRateInputValue = document.getElementById('playbackRateInputValue');
+
+  playbackRateInput.addEventListener('input', (e) => {
+    const target = e.target;
+    const value = target.value;
+
+    squareAnimation.updatePlaybackRate(value);
+    playbackRateInputValue.value = value;
+  });
+});
