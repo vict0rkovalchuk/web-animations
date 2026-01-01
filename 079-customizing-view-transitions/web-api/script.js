14451276d73afacd17457c8b3e502f61816e0da7
Victor Kovalchuk <vict02r05@gmail.com>
2026-01-01 14:35:19 +0200

    lecture 79

---
+++ 079-customizing-view-transitions/web-api/script.js
@@ -0,0 +1,53 @@
+document.addEventListener('DOMContentLoaded', async () => {
+  const element = document.querySelector('.square');
+  const animations = [];
+
+  const addNewAnimation = () => {
+    const squareAnimation = element.animate(
+      [
+        {
+          transform: 'translateX(0)',
+          easing: 'ease-in'
+        },
+        {
+          backgroundColor: 'blue',
+          offset: 0.8,
+          composite: 'replace'
+        },
+        {
+          transform: 'translateX(calc(100vw - 100px)) rotate(360deg)',
+          backgroundColor: 'crimson',
+        }
+      ],
+      {
+        duration: 3000,
+        // delay: 1000,
+        direction: 'alternate',
+        fill: 'both',
+        iterations: 1,
+        easing: 'linear',
+        composite: 'replace',
+        iterationComposite: 'accumulate',
+        timeline: document.timeline
+      }
+    );
+
+    squareAnimation.persist();
+    squareAnimation.addEventListener('remove', (e) => {
+      console.log('Animation Removed by the browser', e);
+    });
+    animations.push(squareAnimation);
+  }
+
+  const addAnimationButton = document.getElementById('add-animation-button');
+  const logAnimationButton = document.getElementById('log-animations-button');
+
+  addAnimationButton.addEventListener('click', () => {
+    addNewAnimation();
+  });
+
+  logAnimationButton.addEventListener('click', () => {
+    console.log(animations);
+    console.log(element.getAnimations());
+  });
+});
