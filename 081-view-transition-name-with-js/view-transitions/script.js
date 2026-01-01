50fdcc814630aeef3d735dfc52b1e0ff1fa6e0c4
Victor Kovalchuk <vict02r05@gmail.com>
2026-01-01 15:11:30 +0200

    lecture 81

---
+++ 081-view-transition-name-with-js/view-transitions/script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const header = document.querySelector(".header");
+  const gridOuter = document.querySelector(".grid-outer");
+  const main = document.querySelector(".main .item");
+  const gridButton = document.querySelector(".grid-view-button");
+
+  function expandImage(item) {
+    const title = item.querySelector("h3").innerText;
+    const largeImage = item.dataset.largeImage;
+    main.querySelector("h2").innerText = title;
+    main.querySelector("img").src = largeImage;
+    gridButton.style.display = "block";
+    header.classList.add("expanded");
+    gridOuter.classList.add("expanded");
+
+    grid
+      .querySelectorAll(".active")
+      .forEach((e) => e.classList.remove("active"));
+    item.classList.add("active");
+
+    grid.style.viewTransitionName = 'grid';
+  }
+
+  function displayGrid() {
+    document.documentElement.scrollTop = 0;
+    grid.style.viewTransitionName = 'none';
+    gridButton.style.display = "none";
+    gridOuter.classList.remove("expanded");
+    header.classList.remove("expanded");
+    grid
+      .querySelectorAll(".active")
+      .forEach((e) => e.classList.remove("active"));
+  }
+
+  grid.addEventListener("click", async (e) => {
+    const item = e.target.closest(".grid-item");
+    if (!item || item.classList.contains("active")) return;
+
+    if(!document.startViewTransition) {
+      expandImage(item);
+      return;
+    }
+
+    document.startViewTransition(() => {
+      expandImage(item);
+    });
+  });
+  
+  gridButton.addEventListener("click", async (e) => {
+    if(!document.startViewTransition) {
+      displayGrid();
+      return;
+    }
+
+    document.startViewTransition(() => {
+      displayGrid();
+    });
+  });
+});
