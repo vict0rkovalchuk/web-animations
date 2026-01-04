c9bac27131d10578621dc6513336662df7374cd3
Victor Kovalchuk <vict02r05@gmail.com>
2026-01-04 14:59:02 +0200

    lecture 103

---
+++ 103-document-view-transitions-example/view-transitions-document/script.js
@@ -0,0 +1,36 @@
+function getPhotoId(_url) {
+  const url = new URL(_url);
+  const arr = url.pathname.split('/');
+  return arr.includes('photo') && arr.at(-1).split('.')[0];
+}
+
+window.addEventListener('pageswap', async (e) => {
+  if (e.viewTransition) {
+    const photoId = getPhotoId(e.activation.entry.url);
+    console.log(photoId);
+    if (photoId) {
+      const grid = document.querySelector('.grid');
+      const item = grid.children[+photoId - 1];
+      item.querySelector('img').style.viewTransitionName = 'image';
+
+      await e.viewTransition.finished;
+      item.querySelector('img').style.viewTransitionName = 'none';
+    }
+  }
+});
+
+window.addEventListener('pagereveal', async (e) => {
+  if (e.viewTransition) {
+    const fromURL = navigation.activation.from.url;
+    const currentURL = navigation.activation.entry.url;
+
+    if (!getPhotoId(currentURL) && getPhotoId(fromURL)) {
+      const grid = document.querySelector('.grid');
+      const item = grid.children[+getPhotoId(fromURL) - 1];
+      item.querySelector('img').style.viewTransitionName = 'image';
+
+      await e.viewTransition.finished;
+      item.querySelector('img').style.viewTransitionName = 'none';
+    }
+  }
+});
